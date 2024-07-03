033bb160500b4b550d7f6b64fa191fbbe66ffac5
SatoKanata <230140@jc-21.jp>
2024-07-03 11:43:33 +0900

    7/3

---
+++ 課題16/Figure.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//Šî–{‚Æ‚È‚éƒNƒ‰ƒX
+class Figure
+{
+public:
+	float teihen, takasa, menseki;
+public:
+	void SetTeihen(float f);
+	void SetTakasa(float f);
+	void Disp();
+};
